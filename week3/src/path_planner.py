c21a384facaaa916175e3f3c9775712838cbc63e
ARCHIT RUNGTA <54287135+archit2604@users.noreply.github.com>
2020-06-05 00:01:04 +0530

    Add files via upload

---
+++ week3/src/path_planner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Point
+rospy.init_node('path_planner')
+pub=rospy.Publisher('path',Point,queue_size=1)
+rate=rospy.Rate(10)
+point=Point()
+point.x=0
+point.y=1
+point.z=0
+while not rospy.is_shutdown():
+    pub.publish(point)
+    rate.sleep()
