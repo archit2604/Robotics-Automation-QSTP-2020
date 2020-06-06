57aa58d6d677449fbd13c90f4dbb1b443c5c87ba
ARCHIT RUNGTA <54287135+archit2604@users.noreply.github.com>
2020-06-06 18:55:11 +0530

    Update ang_vel_server.py

--- tb_task/src/ang_vel_server.py
+++ tb_task/src/ang_vel_server.py
@@ -4,7 +4,7 @@ from tb_task.srv import AngVel
 def callback(request):
     ang_vel=0
     if request.radius!=0:
-        ang_vel=1/request.radius
+        ang_vel=0.2/request.radius
     return ang_vel
 rospy.init_node('AngVelServer')
 service=rospy.Service('compute_ang_vel',AngVel,callback)
